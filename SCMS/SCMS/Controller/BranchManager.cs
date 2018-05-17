ffc91e5e132d537d80bda8be2cc4cf3f2434d1fa
aycanozmen <37209459+aycanozmen@users.noreply.github.com>
2018-05-18 01:51:12 +0300

    Create BranchManager.cs
    BranchManager is added

---
+++ SCMS/SCMS/Controller/BranchManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCMS.Controller
+{
+    public class BranchManager : User
+    {
+        string _branchName;
+        public BranchManager(int id, string name, string surname, string username, string password, string role, string phone, string gender, string birthDate) : base(id, name, surname, username, password, role, phone, gender, birthDate)
+        {
+        }
+
+
+        public string BranchName
+        {
+            get
+            {
+                return _branchName;
+            }
+
+            set
+            {
+                _branchName = value;
+            }
+        }
+    }
+}
