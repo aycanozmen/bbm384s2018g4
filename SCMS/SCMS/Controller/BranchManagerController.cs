219c7b304df05987fd04725397a0bb9cca6b2956
sulekaraslar <32934940+sulekaraslar@users.noreply.github.com>
2018-05-20 01:28:22 +0300

    Update BranchManagerController.cs

--- SCMS/SCMS/Controller/BranchManagerController.cs
+++ SCMS/SCMS/Controller/BranchManagerController.cs
@@ -7,5 +7,25 @@ namespace SCMS.Controller
 {
     public class BranchManagerController
     {
+        
+         public static bool DeleteBranchManager(int id)
+        {
+            bool result = false;
+            using (var connection = Database.GetConnection())
+            {
+
+                var command = new MySqlCommand("DELETE FROM branchmanager WHERE b_id=@Id", connection);
+                command.Parameters.Add(new MySqlParameter("Id", id));
+                connection.Open();
+                if (command.ExecuteNonQuery() != -1)
+                {
+                    result = true;
+                }
+                connection.Close();
+
+
+            }
+            return result;
+        }
     }
 }
