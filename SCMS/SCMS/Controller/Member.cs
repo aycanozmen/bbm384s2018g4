88aed3d9e08edcf40b2c9093d330f4ba3d1a2cc0
ilaydakaya18 <37209879+ilaydakaya18@users.noreply.github.com>
2018-05-21 10:34:43 +0300

    Add files via upload

---
+++ SCMS/SCMS/Controller/Member.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SCMS.Controller
+{
+    public class Member : User
+    {
+        double _height;
+        double _weight;
+        double _waistline;
+        double _arm;
+        double _leg;
+        double _shoulder;
+        double _chest;
+        double _bodyFat;
+        int _active;
+        int _attendance;
+
+        public Member(int id, string name, string surname, string username, string password, string role, string phone, string gender, string birthDate, double height, double weight, double arm, double waistline, double leg, double chest, double shoulder, double bodyFat, int active) : base(id, name, surname, username, password, role, phone, gender, birthDate)
+        {
+
+            this._height = height;
+            this._weight = weight;
+            this._arm = arm;
+            this._bodyFat = bodyFat;
+            this._active = active;
+
+        }
+
+        public double Shoulder
+        {
+            get
+            {
+                return _shoulder;
+            }
+
+            set
+            {
+                _shoulder = value;
+            }
+        }
+
+        public int Attendance
+        {
+            get
+            {
+                return _attendance;
+            }
+
+            set
+            {
+                _attendance = value;
+            }
+        }
+
+        public double Leg
+        {
+            get
+            {
+                return _leg;
+            }
+
+            set
+            {
+                _leg = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return _height;
+            }
+
+            set
+            {
+                _height = value;
+            }
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return _weight;
+            }
+
+            set
+            {
+                _weight = value;
+            }
+        }
+
+        public double Waistline
+        {
+            get
+            {
+                return _waistline;
+            }
+
+            set
+            {
+                _waistline = value;
+            }
+        }
+
+        public double Arm
+        {
+            get
+            {
+                return _arm;
+            }
+
+            set
+            {
+                _arm = value;
+            }
+        }
+
+        public double Chest
+        {
+            get
+            {
+                return _chest;
+            }
+
+            set
+            {
+                _chest = value;
+            }
+        }
+
+        public double BodyFat
+        {
+            get
+            {
+                return _bodyFat;
+            }
+
+            set
+            {
+                _bodyFat = value;
+            }
+        }
+
+        public int Active
+        {
+            get
+            {
+                return _active;
+            }
+
+            set
+            {
+                _active = value;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ SCMS/SCMS/Controller/MemberController.cs
@@ -0,0 +1,131 @@
+﻿using DataAccess;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SCMS.Controller
+{
+
+        public class MemberController
+        {
+            public static bool InsertMember(Member user)
+            {
+                bool result = false;
+                using (var connection = Database.GetConnection())
+                {
+                    var command = new MySqlCommand("INSERT INTO users(name, surname, username, password, role, phone,gender,birthdate) VALUES( @name, @surname, @username, @password, @role, @phone, @gender, @birthdate)", connection);
+
+                    command.Parameters.Add(new MySqlParameter("name", user.Name));
+                    command.Parameters.Add(new MySqlParameter("surname", user.Surname));
+                    command.Parameters.Add(new MySqlParameter("username", user.Username));
+                    command.Parameters.Add(new MySqlParameter("password", user.Password));
+                    command.Parameters.Add(new MySqlParameter("role", "user"));
+                    command.Parameters.Add(new MySqlParameter("phone", user.Phone));
+                    command.Parameters.Add(new MySqlParameter("gender", user.Gender));
+                    command.Parameters.Add(new MySqlParameter("birthdate", user.BirthDate));
+                    connection.Open();
+                    if (command.ExecuteNonQuery() != -1)
+                    {
+                        result = true;
+                    }
+                    connection.Close();
+                }
+                return result;
+            }
+
+        public static bool InsertPhysicalProperty(Member user)
+        {
+            bool result = false;
+            using (var connection = Database.GetConnection())
+            {
+                var command = new MySqlCommand("INSERT INTO members (height, weight, waistline, arm, leg, shoulder, chest, bodyFat, active) VALUES( @height, @weight, @waistline, @arm, @leg, @shoulder, @chest, @bodyFat, @active)", connection);
+
+                command.Parameters.Add(new MySqlParameter("height", user.Height));
+                command.Parameters.Add(new MySqlParameter("weight", user.Weight));
+                command.Parameters.Add(new MySqlParameter("waistline", user.Waistline));
+                command.Parameters.Add(new MySqlParameter("arm", user.Arm));
+                command.Parameters.Add(new MySqlParameter("leg", user.Leg));
+                command.Parameters.Add(new MySqlParameter("shoulder", user.Shoulder));
+                command.Parameters.Add(new MySqlParameter("chest", user.Chest));
+				command.Parameters.Add(new MySqlParameter("bodyFat", user.BodyFat));
+				command.Parameters.Add(new MySqlParameter("active", user.Active));
+
+
+                connection.Open();
+                if (command.ExecuteNonQuery() != -1)
+                {
+                    result = true;
+                }
+                connection.Close();
+            }
+            return result;
+        }
+
+
+        public static bool UpdatePhysicalProperty(Member user)
+            {
+                bool result = false;
+                using (var connection = Database.GetConnection())
+                {
+                    var command = new MySqlCommand("UPDATE members SET height=@height, weight=@weight, waistline=@waistline, arm=@arm, leg=@leg, shoulder=@shoulder, chest=@chest, bodyFat=@bodyFat, active=@active WHERE user_id=@id", connection);
+
+                command.Parameters.Add(new MySqlParameter("height", user.Height));
+                command.Parameters.Add(new MySqlParameter("weight", user.Weight));
+                command.Parameters.Add(new MySqlParameter("waistline", user.Waistline));
+                command.Parameters.Add(new MySqlParameter("arm", user.Arm));
+                command.Parameters.Add(new MySqlParameter("leg", user.Leg));
+                command.Parameters.Add(new MySqlParameter("shoulder", user.Shoulder));
+                command.Parameters.Add(new MySqlParameter("chest", user.Chest));
+				command.Parameters.Add(new MySqlParameter("bodyFat", user.BodyFat));
+				command.Parameters.Add(new MySqlParameter("active", user.Active));
+
+                    command.Parameters.Add(new MySqlParameter("id", user.Id));    //??????????????????????????????????
+
+                    connection.Open();
+                    if (command.ExecuteNonQuery() != -1)
+                    {
+                        result = true;
+                    }
+                    connection.Close();
+                }
+                return result;
+            }
+        public static bool UpdateAttandanceList(Member user)
+        {
+            bool result = false;
+            using (var connection = Database.GetConnection())
+            {
+                var command = new MySqlCommand("SELECT attendance FROM members WHERE  user_id=@id", connection);
+                command.Parameters.Add(new MySqlParameter("id", user.Id));
+
+                MySqlDataReader reader = command.ExecuteReader();
+                
+                if (reader.Read())
+                {
+                    int attendance = Convert.ToInt32(reader["attendance"]);
+                    attendance =attendance+1;
+                }
+                
+
+                var command2 = new MySqlCommand("UPDATE members SET  attendance=@attendance WHERE user_id=@id", connection);
+                command2.Parameters.Add(new MySqlParameter("user_id", user.Id));
+                command2.Parameters.Add(new MySqlParameter("attendance", user.Attendance));
+
+
+                connection.Open();
+                if (command.ExecuteNonQuery() != -1)
+                {
+                    result = true;
+                }
+                connection.Close();
+            }
+            return result;
+        }
+
+
+
+        }
+    }
