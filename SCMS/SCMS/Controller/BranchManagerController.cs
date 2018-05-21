e3245b67cfc68c343eb56daf94fcfbee80b03a88
aycanozmen <37209459+aycanozmen@users.noreply.github.com>
2018-05-21 16:29:30 +0300

    Update BranchManagerController.cs
    Add update function.

--- SCMS/SCMS/Controller/BranchManagerController.cs
+++ SCMS/SCMS/Controller/BranchManagerController.cs
@@ -27,5 +27,36 @@ namespace SCMS.Controller
             }
             return result;
         }
+        
+        
+        public static bool UpdateBranchManager(BranchManager user)
+        {
+            bool result = false;
+            using (var connection = Database.GetConnection())
+            {
+                var command = new MySqlCommand("UPDATE users SET name=@name, surname=@surname, username=@username, password=@password, role=@role, phone=@phone, gender=@gender, birthdate=@birthdate WHERE id=@id", connection);
+                var command2 = new MySqlCommand("UPDATE branch_managers SET b_name=@name  WHERE user_id=@id", connection);
+
+                command.Parameters.Add(new MySqlParameter("name", user.Name));
+                command.Parameters.Add(new MySqlParameter("surname", user.Surname));
+                command.Parameters.Add(new MySqlParameter("username", user.Username));
+                command.Parameters.Add(new MySqlParameter("password", user.Password));
+                command.Parameters.Add(new MySqlParameter("role", "user"));
+                command.Parameters.Add(new MySqlParameter("phone", user.Phone));
+                command.Parameters.Add(new MySqlParameter("gender", user.Gender));
+                command.Parameters.Add(new MySqlParameter("birthdate", user.BirthDate));
+                command.Parameters.Add(new MySqlParameter("user_id", user.Id));
+                command2.Parameters.Add(new MySqlParameter("user_id", user.Id));
+                command2.Parameters.Add(new MySqlParameter("b_name", user.BranchName));
+
+                connection.Open();
+                if (command.ExecuteNonQuery() != -1 && command2.ExecuteNonQuery() != -1)
+                {
+                    result = true;
+                }
+                connection.Close();
+            }
+            return result;
+        }
     }
 }
